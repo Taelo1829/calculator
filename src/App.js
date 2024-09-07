92f8f37e987360c6d57ffa883a6b06c631b9550a
Taelo Seholoba <tseholoba2@gmail.com>
2024-09-08 01:22:12 +0200

    started working on the logic

--- src/App.js
+++ src/App.js
@@ -3,14 +3,54 @@ import "./App.css";
 
 function App() {
   const [value, setValue] = useState("");
+  const [total, setTotal] = useState("");
+
+  function updateCalculations(val) {
+    switch (val) {
+      case "%":
+        if (value !== "" && total !== "") {
+          setValue((prevValue) => prevValue + val);
+          console.log(total);
+          setTotal((prevValue) => (prevValue / 100).toFixed(2));
+        } else {
+          setValue(val);
+          setTotal("Error");
+        }
+        break;
+      default:
+        if (value !== "" && total !== "") {
+          setTotal(eval(value + val));
+          setValue((prevValue) => prevValue + val);
+        } else {
+          setValue(val);
+          setTotal(val);
+        }
+    }
+  }
+
   return (
     <div className="calculator">
       <div className="calculator-container">
         <div className="calculator-screen">
-          <div className="blinking-cursor">|</div>
-          <div>{value}</div>
+          <div className="value">
+            <div>{value}</div>
+            <div className="blinking-cursor">|</div>
+          </div>
+          <div>{total}</div>
         </div>
         <div className="calculator-body">
+          <div className="bottomRow">
+            <div>
+              <div className="row">
+                <div className="button">MC</div>
+                <div className="button">M+</div>
+                <div className="button">M-</div>
+              </div>
+            </div>
+            <div className="operations">
+              <div className="button">MR</div>
+            </div>
+          </div>
           <div className="bottomRow">
             <div>
               <div className="row">
@@ -23,7 +63,7 @@ function App() {
             </div>
             <div className="operations">
               <div className="button">
-                -<div></div>
+                <img src="/images/backspace.png" />
               </div>
             </div>
           </div>
@@ -56,14 +96,22 @@ function App() {
           <div className="bottomRow">
             <div>
               <div className="row">
-                <div className="button">1</div>
+                <div className="button" onClick={() => updateCalculations("1")}>
+                  1
+                </div>
                 <div className="button">2</div>
                 <div className="button">3</div>
               </div>
               <div className="row">
-                <div className="button">%</div>
-                <div className="button">0</div>
-                <div className="button">.</div>
+                <div className="button" onClick={() => updateCalculations("%")}>
+                  %
+                </div>
+                <div className="button" onClick={() => updateCalculations("0")}>
+                  0
+                </div>
+                <div className="button" onClick={() => updateCalculations(".")}>
+                  .
+                </div>
               </div>
             </div>
             <div className="equal-sign">=</div>
